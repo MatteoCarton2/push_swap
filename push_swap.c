71cbf0b92d2ab6c6f3f154b1a8661dcb6401315f
Mattéo <mcarton@student.s19.be>
2025-02-10 17:19:17 +0100

    Initialisation de la stack, ajout de tous les arguments dedans (+ print temporaire pour vérifier)

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 15:12:40 by mcarton           #+#    #+#             */
-/*   Updated: 2025/02/09 17:05:55 by mcarton          ###   ########.fr       */
+/*   Updated: 2025/02/10 17:08:32 by mcarton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,26 @@
 
 int	main(int argc, char **argv)
 {
-    size_t i;
+	size_t	i;
 
-    if (argc < 2)
-        return (0);
-    i = 1;
-    while (i < (size_t)argc)
-    {
-        if (is_valid_args(argv[i]) == 0)
-        {
-             printf("❌");
-            return (0);
-        }
-        i ++;
-    }
-    if (has_duplicates(argc, argv) == 1)
-        return (0);
-    printf("✅");
+	if (argc < 2)
+		return (0);
+	i = 1;
+	while (i < (size_t)argc)
+	{
+		if (is_valid_args(argv[i]) == 0)
+		{
+			printf("INVALIDE ❌\n");
+			return (0);
+		}
+		i++;
+	}
+	if (has_duplicates(argc, argv) == 1)
+	{
+		printf("DOUBLON ❌\n");
+		return (0);
+	}
+	printf("VALIDE ✅\n");
+	initialize_stack(argc, argv);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,15 +6,16 @@
 /*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 15:17:19 by mcarton           #+#    #+#             */
-/*   Updated: 2025/02/09 17:35:30 by mcarton          ###   ########.fr       */
+/*   Updated: 2025/02/10 17:14:55 by mcarton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stddef.h>
-#include <stdio.h>
+# include "libft/libft.h"
+# include <stddef.h>
+# include <stdio.h>
 
 typedef struct s_stack
 {
@@ -24,5 +25,11 @@ typedef struct s_stack
 	struct s_stack	*previous;
 }					t_stack;
 
-int is_valid_args(char *str);
-int has_duplicates(int argc, char **argv);
+int					is_valid_args(char *str);
+int					has_duplicates(int argc, char **argv);
+void				print_stack(t_stack *stack);
+t_stack				*initialize_stack(int argc, char **argv);
+void				free_stack(t_stack *stack);
+t_stack				*add_to_stack(t_stack *first, long nbr);
+
+#endif
